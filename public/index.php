a793d7e5bca7262d2d936b81f1a228b0f099254b
Danzikks <denoman1233@gmail.com>
2025-06-24 22:08:04 +0300

    Меняю структуру проекта на MVC

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 
 if ($_SERVER['REQUEST_URI'] === '/') {
-    require_once __DIR__ . '/site/html/main.html';
+    require_once __DIR__ . '/../app/views/main.html';
 } elseif ($_SERVER['REQUEST_URI'] === '/about') {
-    require_once __DIR__ . '/site/html/about.html';
+    require_once __DIR__ . '/../app/views/about.html';
 }
