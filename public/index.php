53c27377c1ef819499b9adb0b09458a5f9bffc44
Danzikks <denoman1233@gmail.com>
2025-06-27 10:38:13 +0300

    реализовал роутер по пути

--- public/index.php
+++ public/index.php
@@ -2,10 +2,16 @@
 declare(strict_types=1);
 
 
-if ($_SERVER['REQUEST_URI'] === '/') {
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+if ($uri === '/') {
     require_once __DIR__ . '/../app/controllers/main.php';
-} elseif ($_SERVER['REQUEST_URI'] === '/about') {
+} elseif ($uri === '/about') {
     require_once __DIR__ . '/../app/controllers/about.php';
-} elseif ($_SERVER['REQUEST_URI'] === '/events') {
+} elseif ($uri === '/events') {
     require_once __DIR__ . '/../app/controllers/events.php';
+} else {
+    http_response_code(404);
+    echo '404 - Page not found.';
 }
+
