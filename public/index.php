e1554672d4100c969e409dc2f8eff3c2a217e84f
daniilprusakov <prusakovdany@ya.ru>
2025-06-28 15:27:37 +0300

    поправил пути до контроллеров

--- public/index.php
+++ public/index.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 if ($uri === '/') {
-    require_once __DIR__ . '/../app/controllers/main.php';
+    require_once __DIR__ . '/../src/controllers/main.php';
 } elseif ($uri === '/about') {
-    require_once __DIR__ . '/../app/controllers/about.php';
+    require_once __DIR__ . '/../src/controllers/about.php';
 } elseif ($uri === '/events') {
-    require_once __DIR__ . '/../app/controllers/events.php';
+    require_once __DIR__ . '/../src/controllers/events.php';
 } else {
     http_response_code(404);
     echo '404 - Page not found.';
